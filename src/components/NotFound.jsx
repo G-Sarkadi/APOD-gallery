a4d6032d372634071c9859b6dc49798a23565a66
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-10 21:13:53 +0100

    Update error page graphics

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,12 +2,16 @@ import NavButton from "./NavButton";
 
 const NotFound = () => {
     return (
-        <div className="errorContainer">
-            <h1>Oops! You seem to be lost.</h1>
-            <p>Sorry, the page you're looking for doesn't exists.</p>
-            <img className="errorImage" src="/error404.png" alt="error logo"/>
-            <NavButton text="Home" route={"/"} />
-        </div>
+        <>
+            <div className="errorPage">
+                <div className="errorContainer">
+                    <h1>Oops! You seem to be lost.</h1>
+                    <p>Sorry, the page you're looking for doesn't exists.</p>
+                    <img className="errorImage" src="/error404.png" alt="error logo" />
+                    <NavButton text="Home" route={"/"} />
+                </div>
+            </div>
+        </>
     )
 }
 
