24516629f4c4228a5b79f2a8a883a2f4298f40c0
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-26 11:35:59 +0100

    Fix infinite scrolling

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -11,6 +11,7 @@ const Gallery = () => {
 
     const NUMBER_OF_CARDS = 20;
     const URL = `/api?count=${NUMBER_OF_CARDS}&thumbs=true`
+    const VIEWPORT_PERCENT_TO_TRIGGER_NEW_FETCH = 0.8
 
     document.title = 'Picture Gallery'
 
@@ -23,7 +24,7 @@ const Gallery = () => {
             const res = await fetch(URL, { signal: signal })
             const data = await res.json()
             setCards(prevData => {
-                if (prevData){
+                if (prevData) {
                     return [...prevData, ...data]
                 }
                 return data
@@ -40,27 +41,27 @@ const Gallery = () => {
         };
     }, [URL, toggleRefresh]);
 
-    const onScroll = () => {
-        const scrollTop = document.documentElement.scrollTop
-        const scrollHeight = document.documentElement.scrollHeight
-        const clientHeight = document.documentElement.clientHeight
-
-        if (scrollTop + clientHeight >= scrollHeight) {
-            setToggleRefresh(prev => !prev)
-        }
-    }
-
     useEffect(() => {
+        const onScroll = () => {
+            const scrollTop = document.documentElement.scrollTop
+            const scrollHeight = document.documentElement.scrollHeight
+            const clientHeight = document.documentElement.clientHeight
+            // Triggers a new fetch if there isn't a running one and the user scrolls below 80% of the viewport
+            if (!loading && scrollTop + clientHeight >= scrollHeight * VIEWPORT_PERCENT_TO_TRIGGER_NEW_FETCH) {
+                setToggleRefresh(prev => !prev)
+            }
+        }
+
         window.addEventListener('scroll', onScroll)
         return () => window.removeEventListener('scroll', onScroll)
-    }, [cards])
+    }, [loading])
 
     return (
         <>
             {modalOpen && <Modal setOpenModal={setModalOpen} modalContent={modalContent} />}
             <div className="contentContainer">
                 <h3>Gallery</h3>
-                <GalleryContainer galleryContent={cards} setModalOpen={setModalOpen} setModalContent={setModalContent} loading={loading} numberOfCards={NUMBER_OF_CARDS}/>
+                <GalleryContainer galleryContent={cards} setModalOpen={setModalOpen} setModalContent={setModalContent} loading={loading} numberOfCards={NUMBER_OF_CARDS} />
             </div>
         </>
     )
