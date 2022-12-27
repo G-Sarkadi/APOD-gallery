f0ecf7ef9af3f4fb84bf9611a93854090e2234aa
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-27 15:33:05 +0100

    Fix navbar buttons on mobile size

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -8,10 +8,10 @@ const MyNavbar = () => {
                 <Navbar.Brand as={NavLink} to="/">Astronomy Picture of the Day</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto" variant="pills" defaultActiveKey="/">
-                        <Nav.Link as={NavLink} to="/" eventKey="0" >Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/gallery" eventKey="1">Gallery</Nav.Link>
-                        <Nav.Link className="navButton" as={NavLink} to="/about" eventKey="2">About</Nav.Link>
+                    <Nav className="ms-auto" variant="pills" defaultActiveKey="/">
+                        <Nav.Link className="px-3" as={NavLink} to="/" eventKey="0" >Home</Nav.Link>
+                        <Nav.Link className="px-3" as={NavLink} to="/gallery" eventKey="1">Gallery</Nav.Link>
+                        <Nav.Link className="px-3" as={NavLink} to="/about" eventKey="2">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
