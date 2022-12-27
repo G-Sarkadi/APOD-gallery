b9b33c6778a0982d33acfd659f294a186e83dae7
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-27 14:40:02 +0100

    Add spinner as a fallback for lazy loading components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router-dom"
 import Footer from "./Footer"
 import MyNavbar from "./MyNavbar"
 import { Suspense } from "react"
+import Spinner from "./Spinner"
 
 const Layout = ({ props }) => {
     return (
         <>
             <MyNavbar />
-            <Suspense>
+            <Suspense fallback={<h3 style={{ marginTop: "7vh", marginLeft: "45vw" }}><Spinner/></h3>}>
                 <Outlet props={props} />
             </Suspense>
             <Footer />
