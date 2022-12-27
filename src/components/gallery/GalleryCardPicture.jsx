22829bb33c9a6d2c9949efd096dcee790e6d408c
Gergely Sarkadi <gergely.sarkadi@gmail.com>
2022-12-27 13:51:10 +0100

    Hide gallery pictures during loading

--- src/components/gallery/GalleryCardPicture.jsx
+++ src/components/gallery/GalleryCardPicture.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 
 const GalleryCardPicture = ({ content }) => {
+    const [className, setClassName] = useState("galleryCardPicture loading");
+
+    const changeStyle = () => {
+        setClassName("galleryCardPicture");
+    };
+
     return (
-        <img className="galleryCardPicture"
+        <img className={className}
             src={content.thumbnail_url || content.url}
             alt={content.title}
             loading="lazy"
+            onLoad={changeStyle}
         />
     )
 }
